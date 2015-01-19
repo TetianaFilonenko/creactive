e353ce091224d3bf62434209d763a84a9008686c
Tania Filonenko <Tania.Filonenko@gmail.com>
2015-01-19 16:17:27 +0200

    change git repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "github.com/TetianaFilonenko/creactive.git"
+set :repository, "https://github.com/TetianaFilonenko/creactive.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
